e853fe651ed31de63509830840f8f2f9379e72df
Viktoria <vikibtk@gmail.com>
2024-10-19 22:04:06 +0200

    added elastic search package and service

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ChatApp.Data;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Elasticsearch
+var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"))
+    .Authentication(new BasicAuthentication("admin1", "password")) 
+    .ServerCertificateValidationCallback(CertificateValidations.AllowAll); // for development purposes only
+
+var client = new ElasticsearchClient(settings);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton(client);
+builder.Services.AddControllers();
 builder.Services.AddDbContext<ChatAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ChatAppContext") ?? throw new InvalidOperationException("Connection string 'ChatAppContext' not found.")));
 
@@ -26,5 +38,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
